6b9d7159e66ef9c2b584859b0752fd9b3e8dddbe
Luis Galeas <galeaspablo@hotmail.com>
2016-10-20 15:33:29 +0100

    Google Client Status Wrapper

---
+++ Services/GoogleClientStatusWrapper.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace FL\GmailDoctrineBundle\Services;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use FL\GmailBundle\Services\GoogleClientStatus;
+use FL\GmailDoctrineBundle\Entity\SyncSetting;
+
+/**
+ * Class GoogleClientStatusWrapper
+ * @package FL\GmailDoctrineBundle\Services
+ *
+ * This class provides a wrapper to interact with
+ * @see \FL\GmailBundle\Services\GoogleClientStatus
+ */
+class GoogleClientStatusWrapper
+{
+    /**
+     * @var GoogleClientStatus
+     */
+    private $clientStatus;
+
+    /**
+     * @var EntityRepository
+     */
+    private $syncSettingRepository;
+
+    /**
+     * @param GoogleClientStatus $clientStatus
+     * @param EntityManager $entityManager
+     * @param string $syncSettingClass
+     */
+    public function __construct(
+        GoogleClientStatus $clientStatus,
+        EntityManager $entityManager,
+        string $syncSettingClass
+    ) {
+        $this->clientStatus = $clientStatus;
+        $this->syncSettingRepository = $entityManager->getRepository($syncSettingClass);
+    }
+
+    /**
+     * @param string $domain
+     * @return bool
+     */
+    public function isAuthenticatedAndUsersAreSetup(string $domain)
+    {
+        $syncSetting = $this->syncSettingRepository->findOneByDomain($domain);
+
+        if (
+            ($this->clientStatus->isAuthenticated() === true) &&
+            ($syncSetting instanceof SyncSetting)
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+}
